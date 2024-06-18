51be4323efda9c357c8e7b1a7129ff05101333c8
inspiredoff <vindsf@gmail.com>
2024-06-19 02:02:19 +0300

    dev service and api

--- routers.py
+++ routers.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from fastapi import APIRouter, UploadFile
 from fastapi.responses import FileResponse
+from service import phonebook_service
 
 from IO_file import save_file, read_file
 
@@ -21,41 +22,46 @@ class Phonebook(BaseModel):
     contacts: list[Person]
 
 
-@phonebook_router_v1.post("/api/v1/upload-phonebook")
-async def upload_phonebook(file: UploadFile):
-    return read_file(file.filename)
+@phonebook_router_v1.post("/api/v1/{user_id}/upload-phonebook")
+async def upload_phonebook(file: UploadFile, user_id: int):
+    return await phonebook_service.get_file(filename=file.filename, user_id=user_id)
+
+
+@phonebook_router_v1.get("/api/v1/{user_id}")
+async def get_list_phonebooks(user_id: int):
+    return await phonebook_service.get_list_phonebooks(user_id=user_id)
 
 
 @phonebook_router_v1.get("/api/v1/{user_id}/{phonebook_id}/contacts")
-async def get_phonebook(user_id, phonebook_id):
-    return Phonebook(user_id=user_id, phonebook_id=phonebook_id, contacts=[])
+async def get_contact_phonebook(user_id: int, phonebook_id: int):
+    return await phonebook_service.get_contact_phonebook(user_id=user_id, phonebook_id=phonebook_id)
 
 
 @phonebook_router_v1.get("/api/v1/{user_id}/{phonebook_id}/contacts/{person_id}")
-async def get_contact_person_id():
+async def get_contact_person_id(user_id, phonebook_id, person_id):
     pass
     return
 
 
 @phonebook_router_v1.get("/api/v1/{user_id}/{phonebook_id}/contacts/{last_name}")
-async def get_contact_last_name():
+async def get_contact_last_name(user_id, phonebook_id, last_name):
     pass
     return
 
 
 @phonebook_router_v1.get("/api/v1/{user_id}/{phonebook_id}/contacts/{phone_number}")
-async def get_contact_phone_number():
+async def get_contact_phone_number(user_id, phonebook_id, phone_number):
     pass
     return
 
 
 @phonebook_router_v1.post("/api/v1/{user_id}/{phonebook_id}/contacts")
-async def post():
+async def insert_contact(user_id, phonebook_id):
     pass
     return
 
 
 @phonebook_router_v1.get("/api/v1/{user_id}/{phonebook_id}/downloads")
-async def post_phone_book():
+async def post_phone_book(user_id, phonebook_id):
     file = save_file("phonebook.csv", Phonebook.contacts)
     return FileResponse(path="phonebook.csv", filename="phonebook.csv")
--- service.py
+++ service.py
@@ -1,33 +1,69 @@
-from routers import Phonebook, Person 
-
-async def read_file(filename):
-    async with open(file=filename, mode="r") as f:
-        return await f.read()
-
-async def main():
-    content = await read_file("phon.txt")
-    for line in content.splitlines():
-        if len(line) > 1:
-            list_person = line.split(", ")
-            for i in range(len(list_person)):
-                list_person[i] = list_person[i].replace("\t", "")
-            person = Person(list_person[0], list_person[1], list_person[2], list_person[3])
-            print(person.__str__())
-            await save_file("text.txt", person.__repr__())
+from routers import Phonebook, Person
+from storage.storage import PhonebookStorage
 
 
-    class Service:
-        async def get_file(filename:str, user_id):
-            contacts = []
-            async with open(file=filename, mode="r") as f:
-                file_content = await f.read()
-                for line in content.splitlines():
-                    if len(line)>1:
-                        person_data = [i.replace("\t","") for i in line.split(", ")]
-                            contacts.append(Person(
-                                phonebook_id=1,
-                                last_name=person_data[0],
-                                first_name=person_data[1],
-                                phone_number=person_data[2],
-                                info=person_data[3],
-                                ))
+# async def read_file(filename):
+#     async with open(file=filename, mode="r") as f:
+#         return await f.read()
+#
+# async def main():
+#     content = await read_file("phon.txt")
+#     for line in content.splitlines():
+#         if len(line) > 1:
+#             list_person = line.split(", ")
+#             for i in range(len(list_person)):
+#                 list_person[i] = list_person[i].replace("\t", "")
+#             person = Person(list_person[0], list_person[1], list_person[2], list_person[3])
+#             print(person.__str__())
+#             await save_file("text.txt", person.__repr__())
+
+
+class Service:
+    async def get_file(filename: str, user_id):
+        contacts = []
+        async with open(file=filename, mode="r") as f:
+            file_content = await f.read()
+            for line in file_content.splitlines():
+                if len(line) > 1:
+                    person_data = [i.replace("\t", "") for i in line.split(", ")]
+                    contacts.append(Person(
+                        phonebook_id=1,
+                        last_name=person_data[0],
+                        first_name=person_data[1],
+                        phone_number=person_data[2],
+                        info=person_data[3],
+                    ))
+            for contact in contacts:
+                await PhonebookStorage.insert_contact(contact=contact)
+
+    async def get_contact_phonebook(self, user_id: int, phonebook_id: int) -> list[Person] | str:
+        if PhonebookStorage.get_phonebooks(user_id=user_id) == phonebook_id:
+            return await PhonebookStorage.get_contacts(
+                phonebook_id=phonebook_id
+            )
+        else:
+            return "Phonebook not found"
+
+    async def get_list_phonebooks(self, user_id: int) -> list[Phonebook] | str:
+        return await PhonebookStorage.get_phonebooks(user_id=user_id)
+
+    async def get_contact_last_name(self, user_id: int, phonebook_id: int, last_name: str) -> Person | str:
+        if PhonebookStorage.get_phonebooks(user_id=user_id) == phonebook_id:
+            return await PhonebookStorage.get_contacts_by_last_name(
+                last_name=last_name,
+                phonebook_id=phonebook_id
+            )
+        else:
+            return "Phonebook not found"
+
+    async def get_contact_phone_number(self, user_id: int, phonebook_id: int, phone_number: str) -> str:
+        if PhonebookStorage.get_phonebooks(user_id=user_id) == phonebook_id:
+            return await PhonebookStorage.get_contacts_by_phome_number(
+                phone_number=phone_number,
+                phonebook_id=phonebook_id
+            )
+        else:
+            return "Phonebook not found"
+
+
+phonebook_service = Service()
